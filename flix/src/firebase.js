4a1654d7933be06837c7d124b2240c91e3aa1072
Allan <altonallan47@gmail.com>
2023-04-25 10:29:39 +0200

    Update firebase

---
+++ flix/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBNlqspiTTOniGGXi9Jggs2dIXnvCx9Z8o",
+  authDomain: "flix-clone1.firebaseapp.com",
+  projectId: "flix-clone1",
+  storageBucket: "flix-clone1.appspot.com",
+  messagingSenderId: "233391476343",
+  appId: "1:233391476343:web:00e4c4af464a7ffe6c4072",
+  measurementId: "G-6BTKZX7WSS"
+};
