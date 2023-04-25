091e11e763720df46aac20378a78d7446049bc71
Allan <altonallan47@gmail.com>
2023-04-25 11:44:16 +0200

    homescreen

--- flix/src/HomeScreen.js
+++ flix/src/HomeScreen.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Nav from "./Nav";
 import "./HomeScreen.css";
 
 const HomeScreen = () => {
-  return <div className="homeScreen">HomeScreen</div>;
+  return (
+    <div className="homeScreen">
+      <Nav />
+    </div>
+  );
 };
 
 export default HomeScreen;
